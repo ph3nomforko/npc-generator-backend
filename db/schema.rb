0f551630ec5e7e613cf7a372eaee330d68707409
Codi Kraus <codi.kraus@gmail.com>
2021-11-11 16:10:35 -0600

    migrate initial schema

---
+++ db/schema.rb
@@ -0,0 +1,37 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_11_11_215435) do
+
+  create_table "npcs", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.string "alignment"
+    t.string "appearance"
+    t.string "strong_ability"
+    t.string "weak_ability"
+    t.string "behavior"
+    t.string "plot_key"
+    t.integer "occupation_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["occupation_id"], name: "index_npcs_on_occupation_id"
+  end
+
+  create_table "occupations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "npcs", "occupations"
+end
