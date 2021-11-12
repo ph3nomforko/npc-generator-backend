a995b5fc38214ccdc1f89d1a52a1e521012448db
Codi Kraus <codi.kraus@gmail.com>
2021-11-12 08:29:57 -0600

    add index,create,show methods for occupations controller

--- app/controllers/api/v1/occupations_controller.rb
+++ app/controllers/api/v1/occupations_controller.rb
@@ -1,3 +1,28 @@
 class Api::V1::OccupationsController < ApplicationController
-  
+
+  def index
+    @occupations = Occupation.all
+    render json: @occupations
+  end
+
+  def create
+    @occupation = Occupation.new(occupation_params)
+    if @occupation.save
+      render json: @occupation
+    else
+      render json: {error: 'Error creating occupation.'}
+    end
+  end
+
+  def show
+    @occupation = Occupation.find(params[:id])
+    render json: @occupation
+  end
+
+  private
+
+  def occupation_params
+    params.require(:occupation).permit(:name)
+  end
+
 end
