cb9eb7e140ea7660f04517ce2f6418ab5f883c84
Codi Kraus <codi.kraus@gmail.com>
2021-11-12 08:39:14 -0600

    add create,read,update functions

--- app/controllers/api/v1/npcs_controller.rb
+++ app/controllers/api/v1/npcs_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::NpcsController < ApplicationController
+
+  before_action :set_occupation
+
+  def index
+    @npcs = @occupation.npcs
+    render json: @npcs
+  end
+
+  def create
+    @npc = Npc.new(npc_params)
+    if @npc.save
+      render json: @npc
+    else
+      render json: {error: 'Error creating occupation.'}
+    end
+  end
+
+  def show
+    @npc = Npc.find(params[:id])
+    render json: @npc
+  end
+
+  private
+
+  def set_occupation
+    @occupation = Occupation.find(params[:occupation_id])
+  end
+
+  def npc_params
+    params.require(:occupation).permit(
+      :name,
+      :species,
+      :alignment,
+      :behavior,
+      :strong_ability,
+      :weak_ability,
+      :plot_key,
+      :occupation_id
+    )
+  end
 end
